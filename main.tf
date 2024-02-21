4732bcb7acdb270a3b962e4b0897ff844607180a
Shiva Teja <dasi.s@northeastern.edu>
2024-02-21 18:05:55 -0500

    added firewall rules

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "google_compute_network" "vpc" {
   for_each                        = { for idx, name in var.vpc_names : name => idx }
   name                            = each.key
   auto_create_subnetworks         = false
-  routing_mode                    = "REGIONAL"
+  routing_mode                    = var.routing_mode
   delete_default_routes_on_create = true
 
 }
@@ -39,4 +39,56 @@ resource "google_compute_route" "webapp_route" {
   priority         = 1000
   dest_range       = var.route_dest
 
+}
+
+resource "google_compute_instance" "webapp-instance" {
+  for_each = { for idx, name in var.vpc_names : name => idx }
+  name = "webapp-instance-${each.key}"
+  machine_type = "e2-micro"
+  zone = "us-east1-b"
+  boot_disk {
+    initialize_params {
+      image = "webapp-image"
+      size = 100
+      type = "pd-balanced"
+    }
+  }
+
+  network_interface {
+    network    = google_compute_network.vpc[each.key].self_link
+    subnetwork = google_compute_subnetwork.webapp_subnet[each.key].self_link
+  }
+
+}
+
+resource "google_compute_firewall" "allow-app-port" {
+  for_each = { for idx, name in var.vpc_names : name => idx }
+  name        = "allow-app-port-${each.key}"
+  description = "Allow traffic from the internet to the application port"
+  network     = google_compute_network.vpc[each.key].self_link
+  direction   = "INGRESS"
+  priority    = 1001
+
+  allow {
+    protocol = "tcp"
+    ports    = ["3000"]  
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "deny-ssh" {
+  for_each = { for idx, name in var.vpc_names : name => idx }
+  name        = "deny-ssh-${each.key}"
+  description = "Deny SSH traffic from the internet"
+  network     = google_compute_network.vpc[each.key].self_link
+  direction   = "EGRESS"  
+  priority    = 1000
+
+  deny {
+    protocol = "tcp"
+    ports    = ["22"] 
+  }
+
+  source_ranges = ["0.0.0.0/0"]
 }
