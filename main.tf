cfe83a0023fbc458a6dec943ee64300ded906054
Shiva Teja <dasi.s@northeastern.edu>
2024-03-27 16:49:08 -0400

    added pubsub, cloud fn

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "google" {
   project     = var.project_id
   region      = var.region
-  credentials = file(var.cred_file)
+  credentials = "./cred.json"
 }
 
 resource "google_compute_network" "vpc" {
@@ -207,3 +207,156 @@ resource "google_dns_record_set" "a_record" {
   rrdatas = [google_compute_instance.webapp-instance.network_interface[0].access_config[0].nat_ip]
   depends_on = [ google_compute_instance.webapp-instance ]
 }
+
+# MX record for the domain
+resource "google_dns_record_set" "mx_record" {
+  name         = var.domain
+  type         = "MX"
+  ttl          = var.ttl
+  managed_zone = var.managed_zone
+  rrdatas      = var.MX_values
+}
+
+resource "google_dns_record_set" "txt_record1" {
+  name         = var.domain
+  type         = "TXT"
+  ttl          = var.ttl 
+  managed_zone = var.managed_zone
+  rrdatas      = var.TXT1
+}
+
+resource "google_dns_record_set" "txt_record2" {
+  name         = var.TXT2_Domain
+  type         = "TXT"
+  ttl          = var.ttl
+  managed_zone = var.managed_zone
+  rrdatas      = var.TXT2
+}
+
+resource "google_dns_record_set" "cname_record" {
+  name         = "email.${var.domain}" 
+  type         = "CNAME"
+  ttl          = var.ttl
+  managed_zone = var.managed_zone
+  rrdatas      = var.CNAME_value
+}
+
+
+# pub/sub topic
+resource "google_pubsub_topic" "verify_email_topic" {
+  name = var.pub_sub_topic
+}
+
+# pub/sub subscription
+resource "google_pubsub_subscription" "verify_email_subscription" {
+  name  = var.pub_sub_sub
+  topic = google_pubsub_topic.verify_email_topic.name
+
+  ack_deadline_seconds = 20
+}
+
+# service account: cloud function
+resource "google_service_account" "cloud_function_sa" {
+  account_id   = var.cloud_fn_acc_id
+  display_name = "Cloud Function Mailgun Service Account"
+}
+
+# role: pub/sub subscriber
+resource "google_project_iam_member" "pubsub_subscriber" {
+  project = var.project_id
+  role    = var.pub_sub_subscriber
+  member  = "serviceAccount:${google_service_account.cloud_function_sa.email}"
+}
+
+# role: pub/sub publisher
+resource "google_project_iam_member" "pubsub_publisher" {
+  project = var.project_id
+  role    = var.pub_sub_publisher
+  member  = "serviceAccount:${google_service_account.webapp_service_account.email}"
+}
+
+# role: secret accessor
+resource "google_project_iam_member" "secret_accessor" {
+  project = var.project_id
+  role    = var.secret_accessor
+  member  = "serviceAccount:${google_service_account.cloud_function_sa.email}"
+}
+
+# role: function invoker
+resource "google_project_iam_member" "function_invoker" {
+  project = var.project_id
+  role    = var.function_invoker
+  member  = "serviceAccount:${google_service_account.cloud_function_sa.email}"
+}
+
+# role: function invoker
+resource "google_project_iam_member" "sql_client" {
+  project = var.project_id
+  role    = var.sql_client
+  member  = "serviceAccount:${google_service_account.cloud_function_sa.email}"
+}
+
+# role: service account token creator
+resource "google_project_iam_member" "service_account_token_creator" {
+  project = var.project_id
+  role    = var.sa_creator
+  member  = "serviceAccount:${google_service_account.cloud_function_sa.email}"
+}
+
+resource "random_id" "bucket_suffix" {
+  byte_length = 2
+}
+resource "google_storage_bucket" "function_source_bucket" {
+  name     = "${var.storage_bucket_name}-${random_id.bucket_suffix.hex}"
+  location = "US"
+}
+
+resource "google_storage_bucket_object" "function_source_zip" {
+  name   = var.storage_object_name
+  bucket = google_storage_bucket.function_source_bucket.name
+  source = "./cloudFn.zip"
+}
+
+# cloud function
+resource "google_cloudfunctions_function" "verify_email_function" {
+  name        = var.cloud_fn_name
+  description = "Function to verify email addresses"
+  runtime     =  var.cloud_fn_runtime
+
+  available_memory_mb   = 256
+  source_archive_bucket = google_storage_bucket.function_source_bucket.name
+  source_archive_object = google_storage_bucket_object.function_source_zip.name
+
+  event_trigger {
+    event_type = var.cloud_fn_trigger
+    resource = google_pubsub_topic.verify_email_topic.id
+  }
+  vpc_connector = google_vpc_access_connector.default.id
+
+  entry_point = var.cloudfn_entry
+
+  service_account_email = google_service_account.cloud_function_sa.email
+
+  environment_variables  = {
+    DB_HOST                 = google_sql_database_instance.default.private_ip_address
+    DB_NAME                 = var.db_name,
+    DB_PASS                 = random_password.password.result,
+    DB_USER                 = var.db_user,
+    INSTANCE_CONNECTION_NAME = var.instance_connection_name,
+    MAILGUN_API_KEY        = var.mailgun_api,
+    MAILGUN_DOMAIN         = var.domain,
+    SOCKET_PATH            = var.socket_path
+  }
+
+  project = var.project_id
+  region  = var.region
+}
+
+# Serverless VPC Access connector
+resource "google_vpc_access_connector" "default" {
+  name          = var.connector_name
+  region        = var.region
+  network       = google_compute_network.vpc.name
+  ip_cidr_range = var.connector_cidr
+}
+
